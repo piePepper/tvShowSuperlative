0f819f41225fdce2a8e8a20bddf16605447112ba
Bart Yellapu <38868384+bartmann88@users.noreply.github.com>
2020-08-15 12:15:32 -0500

    Initial Commit - Api data displays on Devtools and Created Card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TvShowCard from "./TvShowCard";
+import axios from "axios";
 
 class App extends Component {
-  render(){
+  constructor() {
+    super();
+    this.state = { apiData: [], query: "batman" };
+  }
+
+  componentDidMount() {
+    axios({
+      url: `http://api.tvmaze.com/search/shows?q=${this.state.query}`,
+    }).then((response) => {
+      this.setState({ apiData: response.data }, () => {
+        console.log(this.state.apiData);
+      });
+    });
+  }
+  render() {
     return (
-      <div className="App" >
-        <h1>If you see this then the website works!</h1>
+      <div className="App">
+        <h1>neatFlix</h1>
+        <TvShowCard
+          title="Girls"
+          img="fikjsklfjksd"
+          alt="img is broken"
+          network="HBO"
+          country="India"
+          genre="Comedy"
+          description="Must watch"
+        />
       </div>
-    )
+    );
   }
 }
 
