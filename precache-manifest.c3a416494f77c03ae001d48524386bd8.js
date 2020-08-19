ee58ede90b726335a2ce7ce1083fcb5adc1fde85
Bart Yellapu <38868384+bartmann88@users.noreply.github.com>
2020-08-19 13:24:09 -0500

    Updates

---
+++ precache-manifest.c3a416494f77c03ae001d48524386bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365d81cc8d9d79813e9286bdc309ad88",
+    "url": "/tvShowSuperlative/index.html"
+  },
+  {
+    "revision": "fcc4c4d3acd5d5c23ebf",
+    "url": "/tvShowSuperlative/static/css/main.1aa98fce.chunk.css"
+  },
+  {
+    "revision": "6683e688dae55ef2991b",
+    "url": "/tvShowSuperlative/static/js/2.c2786552.chunk.js"
+  },
+  {
+    "revision": "ed2ca2bc1fcfad87ee81d05297bcf271",
+    "url": "/tvShowSuperlative/static/js/2.c2786552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc4c4d3acd5d5c23ebf",
+    "url": "/tvShowSuperlative/static/js/main.720f3a14.chunk.js"
+  },
+  {
+    "revision": "9f1b295bbe49f1abd175",
+    "url": "/tvShowSuperlative/static/js/runtime-main.2b9d7869.js"
+  }
+]);
