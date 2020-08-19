5d01109ab1c47e32346949e4b19d6d3f35fde176
a970747 <Matt.J.Jackson@gmail.com>
2020-08-19 12:51:43 -0400

    Fix case issue for firebase component

--- src/ListSelection.js
+++ src/ListSelection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import firebase from './firebase';
+import firebase from './Firebase';
 import { Link } from "react-router-dom";
 
 class ListSelection extends Component {
